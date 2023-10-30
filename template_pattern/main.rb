b525c3b1dfc624ed9dca3fc08d85d95ffa26e48c
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-10-30 17:15:54 -0300

    feat: finish main.rb file

--- template_pattern/main.rb
+++ template_pattern/main.rb
@@ -1,6 +1,20 @@
 require './sum.rb'
+require './subtraction.rb'
 
 operands = (1..10).to_a
 
-new_operation = Sum.new(operands)
-new_operation.perform_operation
+# The template method consists of:
+# - A base class that implements the general methods used in the operation/process (Operaion in this case)
+# - Child classes that will change ONLY the variable part of the code (In this case how the operands will be aggregated)
+# - The methods that can be overwritten by the child classes are called Hook Methods
+# - The benefits of this pattern is to remove from the concrete classes (the child ones) all the code not related to its core functionality
+# I.E: The sum class cares not about how to display the data to the user. It only cares about summing the operands passed to it.
+
+# Examples:
+sum_operation = Sum.new(operands)
+sum_operation.perform_operation
+
+puts "\n"
+
+subtraction_operation = Subtraction.new(operands)
+subtraction_operation.perform_operation

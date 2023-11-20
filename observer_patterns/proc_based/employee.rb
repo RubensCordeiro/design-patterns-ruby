e3f5315efac08971c6b491b5ee1f7dad65198d0d
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-11-19 21:39:35 -0300

    add proc based implementation

---
+++ observer_patterns/proc_based/employee.rb
@@ -0,0 +1,26 @@
+class Employee
+  attr_accessor :name, :title
+  attr_reader :salary
+
+  def initialize(name, salary, title, observers = [])
+    @observers = observers
+    @name = name
+    @salary = salary
+    @title = title
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers(:salary)
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  private
+
+  def notify_observers(change)
+    @observers.each { |observer| observer.call(self, change) }
+  end
+end
---
+++ observer_patterns/proc_based/main.rb
@@ -0,0 +1,14 @@
+require_relative './employee'
+
+hr = proc do |employee, change|
+  puts "Employee #{employee.name} has changed its #{change}"
+end
+
+tax_dept = proc do |employee, _change|
+  puts "So, your new salary is #{employee.salary}?? Time to pay taxes."
+end
+
+employee = Employee.new('rubens', 1000, 'SEIII')
+employee.add_observer(hr)
+employee.add_observer(tax_dept)
+employee.salary = 5000
